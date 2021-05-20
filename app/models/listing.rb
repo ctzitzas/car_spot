4165ef3ea713d8870b08769ac33dfb85d3271c32
Chris Tzitzas <ctzitzas@gmail.com>
2021-05-20 17:46:44 +1000

    MODEL: Added enumerators for listing and profile models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,9 @@
 class Listing < ApplicationRecord
   belongs_to :user
   has_one :transaction, dependent: :destroy
-  validates :type, :status, :price, :year, :make, :model, :roadworhty, :condition, :description, presence: true 
+  validates :type, :status, :price, :year, :make, :model, :roadworhty, :condition, :description, presence: true
+  enum type: { selling: 1, wanted: 2, }
+  enum status: { active: 1, inactive: 2, sold: 3 }
+  enum roadworthy: { supplied: 1, pending: 2, none: 3 }
+  enum condition: { poor: 1, good: 2, excellent: 3, pristine: 4 }
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,5 @@ class Profile < ApplicationRecord
   has_one :location
   has_one :user
   validates :first_name, :last_name, :type, presence: true
+  enumm type: { enthusiast: 1, hobbyist: 2, collector: 3, dealer: 4 }
 end
