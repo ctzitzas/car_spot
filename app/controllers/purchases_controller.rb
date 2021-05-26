8e5ce6e572fcc70eef0bf1150e0a4edb33846ac0
Chris Tzitzas <ctzitzas@gmail.com>
2021-05-26 14:42:34 +1000

    Revert "Removeed unused code"
    This reverts commit b62a5a85eb5659b7260ac8d2ecc9f74a8576384a.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -22,4 +22,28 @@ class PurchasesController < ApplicationController
     listing.update(status: 3)
   end
 
+  def buy
+    stripe_session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      client_reference_id: current_user.id,
+      customer_email: current_user.email,
+      line_items:[{
+        amount: (@listing.price * 100).to_i,
+        name: (@listing.year + ' ' + @listing.make + ' ' + @listing.model),
+        currency: 'aud',
+        quantity: 1,
+        images: @listing.images.empty? ? nil : [@listing.images.first.service_url]
+      }],
+      payment_intent_data: {
+        metadata: {
+          listing_id: @listing.id,
+          user_id: current_user.id
+        }
+      },
+      success_url: "#{root_url}/purchases/success?listingId=#{@listing.id}",
+      cancel_url: "#{root_url}listings"
+    )
+    @session_id = stripe_session.id
+  end
+
 end
